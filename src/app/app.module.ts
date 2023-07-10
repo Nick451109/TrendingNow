f921bbe9d402336e02df19a86067f7c761701917
Nick451109 <narevalo@espol.edu.ec>
2023-07-10 13:50:35 -0500

    Template and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './button/button.component';
+import { HomeComponent } from './home/home.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent,
+    HomeComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/button/button.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  template: '<button class="btn btn-primary">Botón Bootstrap</button>'
+})
+export class ButtonComponent {
+
+}
