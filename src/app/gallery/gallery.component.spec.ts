a8ff69e2913c8cc0517be238c2030b2397d12a1f
Nick451109 <narevalo@espol.edu.ec>
2023-07-24 21:56:28 -0500

    component tests

--- src/app/gallery/gallery.component.spec.ts
+++ src/app/gallery/gallery.component.spec.ts
@@ -1,13 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientModule } from '@angular/common/http';
 import { GalleryComponent } from './gallery.component';
+import { GetDataService } from '../providers/get-data.service';
 
+//---------------------------------------------------------
 describe('GalleryComponent', () => {
   let component: GalleryComponent;
   let fixture: ComponentFixture<GalleryComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [ HttpClientModule ],
+      providers: [ GetDataService ],
       declarations: [GalleryComponent]
     });
     fixture = TestBed.createComponent(GalleryComponent);
@@ -19,11 +23,55 @@ describe('GalleryComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it('should request service user after Angular calls ngOnInit', (done: DoneFn) => {
+
+    // Llame a ngOnInit para simular el ciclo de vida del componente
+    component.ngOnInit();
+
+
+    // Utilice fixture.whenStable para esperar a que se resuelva el observable del servicio
+    fixture.whenStable().then(() => {
+      
+
+      // Valide que la respuesta sea mayor que 0
+      expect(component.data.length).toBeGreaterThan(0)
+
+      // Que espere hasta que llegue la respuesta
+      done();
+    });
+  });
+});
+//---------------------------------------------------------
+describe('SoloHTMLComponent', () => {
+  TestBed.configureTestingModule({
+    imports: [ HttpClientModule ],
+    providers: [ GetDataService ],
+    declarations: [GalleryComponent]
+  });
+  let fixture: ComponentFixture<GalleryComponent>;
+  fixture = TestBed.createComponent(GalleryComponent);
+
+  //Valide la existencia de un elemento <nav> en la plantilla html del elemento
+
+  it('nav element', ()=> {
+      const headerElement: HTMLElement = fixture.nativeElement;
+      const nav = headerElement.querySelector('nav')!;
+      expect(nav).toBeTruthy();
+    })
+
+  //Valide la NO existencia de un elemento <p> en la plantilla html del elemento
+
+  it('No p element', ()=> {
+      const headerElement: HTMLElement = fixture.nativeElement;
+      const nav = headerElement.querySelector('p')!;
+      expect(nav).toBeFalsy();
+  })
+
+  
   it('section element', () => {
     const galleryContainer: HTMLElement = fixture.nativeElement;
     const p = galleryContainer.querySelector('section')!;
     expect(p.id).toEqual("gallery");
   });
 
-  
-});
+});	
--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,7 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+  //Importe el módulo cliente para requerimientos http
+  import { HttpClientModule } from '@angular/common/http';
 
+  //Importe el servicio
+  import { GetDataService } from '../providers/get-data.service';
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
