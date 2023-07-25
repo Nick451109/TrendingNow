224576e3ce99eedf2d069d4a25cf386755e2a0a3
Nick451109 <narevalo@espol.edu.ec>
2023-07-24 22:55:22 -0500

    table test fix

--- src/app/table/table.component.spec.ts
+++ src/app/table/table.component.spec.ts
@@ -1,13 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TableComponent } from './table.component';
+import { HttpClientModule } from '@angular/common/http';
 
+//Importe el servicio
+import { GalleryComponent } from '../gallery/gallery.component'; 
 describe('TableComponent', () => {
   let component: TableComponent;
   let fixture: ComponentFixture<TableComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [ HttpClientModule ],
+      providers: [ GalleryComponent ],
       declarations: [TableComponent]
     });
     fixture = TestBed.createComponent(TableComponent);
